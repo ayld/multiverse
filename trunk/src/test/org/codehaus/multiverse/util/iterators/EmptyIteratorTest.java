ad1cc6e1fdc3600440efad6fc71a23c95107919f
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2008-12-14 10:37:57 +0000

    - added the unit tests to the ant script - fixed the emptyiteratortest

--- trunk/src/test/org/codehaus/multiverse/util/iterators/EmptyIteratorTest.java
+++ trunk/src/test/org/codehaus/multiverse/util/iterators/EmptyIteratorTest.java
@@ -1,32 +1,33 @@
 package org.codehaus.multiverse.util.iterators;
 
 import junit.framework.TestCase;
-import org.codehaus.multiverse.util.iterators.EmptyIterator;
+
+import java.util.NoSuchElementException;
 
 public class EmptyIteratorTest extends TestCase {
     private EmptyIterator it;
 
-    public void setUp(){
+    public void setUp() {
         it = EmptyIterator.INSTANCE;
     }
 
-    public void testHasNext(){
+    public void testHasNext() {
         assertFalse(it.hasNext());
     }
 
-    public void testNext(){
-        try{
+    public void testNext() {
+        try {
             it.next();
             fail();
-        }catch(UnsupportedOperationException ex){
+        } catch (NoSuchElementException ex) {
         }
     }
 
-    public void testRemove(){
-         try{
+    public void testRemove() {
+        try {
             it.remove();
             fail();
-        }catch(UnsupportedOperationException ex){
+        } catch (UnsupportedOperationException ex) {
         }
     }
 }
