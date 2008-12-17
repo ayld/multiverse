f247e17f04f5a27d5715ca328f5d67e0a302246a
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2008-12-17 18:20:03 +0000

    - fixed bug with balancing - improved the unit test for balancing.. more real world test

--- trunk/src/main/org/codehaus/multiverse/multiversionedstm/growingheap/HeapTreeNode.java
+++ trunk/src/main/org/codehaus/multiverse/multiversionedstm/growingheap/HeapTreeNode.java
@@ -7,7 +7,11 @@ import static java.lang.Math.max;
 /**
  * A binary node that can be placed in the GrowingMultiversionedHeap.
  * <p/>
- * Searchs have a complexity of O(log n)
+ * Searches have a complexity of O(log n)
+ * <p/>
+ * HeapTreeNodes are immutable.
+ * <p/>
+ * http://en.wikipedia.org/wiki/AVL_tree
  *
  * @author Peter Veentjer.
  */
@@ -22,7 +26,7 @@ public final class HeapTreeNode {
 
     //todo: version should be part of the content.
     private final long version;
-    private int height;
+    private final int height;
 
     public HeapTreeNode(DehydratedStmObject content, long version, HeapTreeNode left, HeapTreeNode right) {
         if (content == null) throw new NullPointerException();
@@ -31,7 +35,23 @@ public final class HeapTreeNode {
         this.content = content;
         this.left = left;
         this.right = right;
-        this.height = max(height(left), height(right)) + 1;
+        this.height = height(left, right);
+    }
+
+    public int height() {
+        return height;
+    }
+
+    private static int height(HeapTreeNode node) {
+        return node == null ? 0 : node.height();
+    }
+
+    private static int height(HeapTreeNode left, HeapTreeNode right) {
+        return max(height(left), height(right)) + 1;
+    }
+
+    private static int balanceFactor(HeapTreeNode left, HeapTreeNode right) {
+        return height(right) - height(left);
     }
 
     public long getHandle() {
@@ -73,8 +93,14 @@ public final class HeapTreeNode {
         return new HeapTreeNode(p.getContent(), p.getVersion(), a, qNew);
     }
 
+    /**
+     * http://en.wikipedia.org/wiki/Tree_rotation
+     *
+     * @return
+     */
     public HeapTreeNode doubleRotateRight() {
-        throw new RuntimeException();
+        HeapTreeNode newLeft = left.singleRotateLeft();
+        return new HeapTreeNode(content, version, newLeft, right).singleRotateRight();
     }
 
     /**
@@ -95,8 +121,14 @@ public final class HeapTreeNode {
         return new HeapTreeNode(q.getContent(), q.getVersion(), pNew, c);
     }
 
+    /**
+     * http://en.wikipedia.org/wiki/Tree_rotation
+     *
+     * @return
+     */
     public HeapTreeNode doubleRotateLeft() {
-        throw new RuntimeException();
+        HeapTreeNode newRight = right.singleRotateRight();
+        return new HeapTreeNode(content, version, left, newRight).singleRotateLeft();
     }
 
     /**
@@ -104,58 +136,72 @@ public final class HeapTreeNode {
      * <p/>
      * todo: tree is not balanced..
      * todo: algorithm is recursive and not iterative
+     * <p/>
+     * http://upload.wikimedia.org/wikipedia/en/c/c4/Tree_Rebalancing.gif
      *
      * @param change
+     * @param changeVersion
      * @return
      */
     public HeapTreeNode createNew(DehydratedStmObject change, long changeVersion) {
-        HeapTreeNode result;
+        HeapTreeNode unbalanced = createUnbalanced(change, changeVersion);
+        return unbalanced.balance();
+
+    }
+
+    private HeapTreeNode balance() {
+        int balanceFactor = balanceFactor();
+        switch (balanceFactor) {
+            case 0:
+                return this;
+            case 1:
+                return this;
+            case -1:
+                return this;
+            case 2:
+                //het is een right/right of een right/left case
+                //is the right right heavy, or left heavy
+                int rightBalanceFactor = right.balanceFactor();
+                if (rightBalanceFactor == 1)
+                    return this.singleRotateLeft();
+                else
+                    return this.doubleRotateLeft();
+            case -2:
+                //is the left/left  heavy, or left/right heavy
+                int leftBalanceFactor = left.balanceFactor();
+                if (leftBalanceFactor == -1)
+                    return this.singleRotateRight();
+                else
+                    return this.doubleRotateRight();
+            default:
+                throw new RuntimeException("unhandeled balanceFactor: " + balanceFactor);
+        }
+    }
+
+    private HeapTreeNode createUnbalanced(DehydratedStmObject change, long changeVersion) {
         switch (compare(change.getHandle())) {
             case COMPARE_SPOT_ON:
                 //since the left and right trees are balanced, the new node will be balanced.
                 return new HeapTreeNode(change, changeVersion, left, right);
             case COMPARE_GO_RIGHT:
-                //the node will be added to the right side.. Is the child going to take care
-                //of it, or should it be attached to this node directly.
-                if (right == null) {
-                    //this node is taking care of the addition.
-                    //adding it to the right would not lead to a large unbalance.
-                    HeapTreeNode newRight = new HeapTreeNode(change, changeVersion, null, null);
-                    return new HeapTreeNode(content, changeVersion, left, newRight);
-                } else {
-                    //the child is taking care of the addition
-                    //it could be that the newTree leads to an unbalanced tree.
-                    HeapTreeNode newRight = right.createNew(change, changeVersion);
-                    int rightHeight = newRight.height();
-                    int leftHeight = height(left);
-                    int balanceFactor = rightHeight - leftHeight;
-                    if (balanceFactor == 0 || balanceFactor == 1)
-                        return new HeapTreeNode(content, changeVersion, left, newRight);
-
-                    HeapTreeNode newLeft = new HeapTreeNode(this.content, this.getVersion(), this.left, newRight.getLeft());
-                    return new HeapTreeNode(newRight.getContent(), newRight.getVersion(), newLeft, newRight.getRight());
-                }
+                HeapTreeNode newRight;
+                if (right == null)
+                    newRight = new HeapTreeNode(change, changeVersion, null, null);
+                else
+                    newRight = right.createNew(change, changeVersion);
+                return new HeapTreeNode(content, version, left, newRight);
             case COMPARE_GO_LEFT:
-                if (left == null) {
-                    HeapTreeNode newLeft = new HeapTreeNode(change, changeVersion, null, null);
-                    return new HeapTreeNode(content, changeVersion, newLeft, right);
-                } else {
-                    //the child is taking care of the addition
-                    //it could be that the newTree leads to an unbalanced tree.
-                    HeapTreeNode newLeft = left.createNew(change, changeVersion);
-                    int leftHeight = newLeft.height();
-                    int rightHeight = height(right);
-                    int balanceFactor = rightHeight - leftHeight;
-                    if (balanceFactor == 0 || balanceFactor == -1)
-                        return new HeapTreeNode(content, changeVersion, newLeft, right);
-
-                    HeapTreeNode newRight = new HeapTreeNode(this.content, this.getVersion(), newLeft.getLeft(), this.right);
-                    return new HeapTreeNode(newLeft.getContent(), newLeft.getVersion(), newLeft.getRight(), newRight);
-                }
+                HeapTreeNode newLeft;
+                if (left == null)
+                    newLeft = new HeapTreeNode(change, changeVersion, null, null);
+                else
+                    newLeft = left.createNew(change, changeVersion);
+                return new HeapTreeNode(content, version, newLeft, right);
             default:
                 throw new RuntimeException();
         }
     }
+
 
     /**
      * Returns the size of this HeapTreeNode. The size will always be equal or larger than 0.
@@ -172,14 +218,6 @@ public final class HeapTreeNode {
         if (left != null)
             size += left.size();
         return size;
-    }
-
-    public int height() {
-        return height;
-    }
-
-    private static int height(HeapTreeNode node) {
-        return node == null ? 0 : node.height();
     }
 
     public int balanceFactor() {
