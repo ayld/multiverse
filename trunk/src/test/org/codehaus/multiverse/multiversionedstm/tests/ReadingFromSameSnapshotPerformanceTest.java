55362953783df078ea166bdc81d9e1a34897a6af
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2009-02-28 21:55:12 +0000

    - added some extra tests

---
+++ trunk/src/test/org/codehaus/multiverse/multiversionedstm/tests/ReadingFromSameSnapshotPerformanceTest.java
@@ -0,0 +1,94 @@
+package org.codehaus.multiverse.multiversionedstm.tests;
+
+import org.codehaus.multiverse.TestThread;
+import static org.codehaus.multiverse.TestUtils.joinAll;
+import static org.codehaus.multiverse.TestUtils.startAll;
+import org.codehaus.multiverse.multiversionedheap.Deflatable;
+import org.codehaus.multiverse.multiversionedheap.DummyDeflatable;
+import org.codehaus.multiverse.multiversionedheap.MultiversionedHeapSnapshot;
+import org.codehaus.multiverse.multiversionedheap.standard.DefaultMultiversionedHeap;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * A performance test that tests the performance of reads on a snapshot.
+ *
+ * @author Peter Veentjer.
+ */
+public class ReadingFromSameSnapshotPerformanceTest {
+    private int heapsize = 2000000;
+    private int readerCount = 2;
+    private long readCount = 10000000;
+
+    private DefaultMultiversionedHeap heap;
+    private MultiversionedHeapSnapshot snapshot;
+    private AtomicLong readCountDown = new AtomicLong();
+    private long[] handles;
+    private long startMs, endMs;
+
+    @Before
+    public void setUp() {
+        heap = new DefaultMultiversionedHeap();
+        fillHeap();
+        snapshot = heap.getActiveSnapshot();
+    }
+
+    @After
+    public void tearDown() {
+        long timeMs = (endMs - startMs) + 1;
+        System.out.println(String.format("%s reads with %s threads and a heapsize of %s took %s ms",
+                readCount, readerCount, heapsize, timeMs));
+        System.out.println(String.format("%s reads/second", readCount / (timeMs / 1000.0)));
+    }
+
+    @Test
+    public void test() {
+        readCountDown.set(readCount);
+        ReaderThread[] readerThreads = createReaderThreads();
+
+        startMs = System.currentTimeMillis();
+        startAll(readerThreads);
+        joinAll(readerThreads);
+        endMs = System.currentTimeMillis();
+    }
+
+    public void fillHeap() {
+        System.out.println("Start filling heap");
+        handles = new long[heapsize];
+        for (int k = 0; k < handles.length; k++) {
+            Deflatable deflatable = new DummyDeflatable("" + k);
+            handles[k] = deflatable.___getHandle();
+            heap.commit(heap.getActiveSnapshot(), deflatable);
+        }
+        System.out.println("Finished filling heap");
+    }
+
+    public ReaderThread[] createReaderThreads() {
+        ReaderThread[] threads = new ReaderThread[readerCount];
+        for (int k = 0; k < threads.length; k++)
+            threads[k] = new ReaderThread(k);
+        return threads;
+    }
+
+    private class ReaderThread extends TestThread {
+
+        public ReaderThread(int id) {
+            super("ReaderThread-" + id);
+
+        }
+
+        @Override
+        public void run() {
+            long x;
+            long y = 0;
+            while ((x = readCountDown.getAndDecrement()) > 0) {
+                long handle = (y + x * 31) % handles.length;
+                snapshot.read(handle);
+                y++;
+            }
+        }
+    }
+}
---
+++ trunk/src/test/org/codehaus/multiverse/multiversionedstm/tests/WorkingOnBareHeapStackTest.java
@@ -0,0 +1,159 @@
+package org.codehaus.multiverse.multiversionedstm.tests;
+
+import org.codehaus.multiverse.TestThread;
+import static org.codehaus.multiverse.TestUtils.joinAll;
+import static org.codehaus.multiverse.TestUtils.startAll;
+import org.codehaus.multiverse.core.RetryError;
+import org.codehaus.multiverse.core.Transaction;
+import org.codehaus.multiverse.multiversionedheap.MultiversionedHeap.CommitResult;
+import org.codehaus.multiverse.multiversionedheap.MultiversionedHeapSnapshot;
+import org.codehaus.multiverse.multiversionedheap.standard.DefaultMultiversionedHeap;
+import org.codehaus.multiverse.multiversionedstm.MultiversionedStm;
+import org.codehaus.multiverse.multiversionedstm.MultiversionedStm.MultiversionedTransaction;
+import org.codehaus.multiverse.multiversionedstm.examples.Stack;
+import org.codehaus.multiverse.util.iterators.InstanceIterator;
+import org.codehaus.multiverse.util.latches.CheapLatch;
+import org.codehaus.multiverse.util.latches.Latch;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class WorkingOnBareHeapStackTest {
+    private MultiversionedStm stm;
+    private DefaultMultiversionedHeap heap;
+
+    private int itemCount = 1000000 * 1;
+
+    private AtomicInteger producerCountdown = new AtomicInteger();
+    private AtomicInteger consumerCountdown = new AtomicInteger();
+    private long handle;
+    private Object item = "foo";
+    private long startMs;
+
+    @Before
+    public void setUp() {
+        heap = new DefaultMultiversionedHeap();
+        stm = new MultiversionedStm(heap);
+        createStack();
+        startMs = System.currentTimeMillis();
+    }
+
+    @After
+    public void tearDown() {
+        long timeMs = (System.currentTimeMillis() - startMs) + 1;
+
+        System.out.println(heap.getStatistics());
+        System.out.println(String.format("%s chain alivecount", heap.getSnapshotChain().getAliveCount()));
+        System.out.println(String.format("%s items took %s ms", itemCount, timeMs));
+        System.out.println(String.format("%s transactions/second", heap.getStatistics().commitSuccessCount.longValue() / (timeMs / 1000.0)));
+    }
+
+    public void createStack() {
+        Transaction t = stm.startTransaction();
+        handle = t.attachAsRoot(new Stack());
+        t.commit();
+    }
+
+    @Test
+    public void test() {
+        producerCountdown.set(itemCount);
+        consumerCountdown.set(itemCount);
+
+        ProducerThread producerThread = new ProducerThread();
+        ConsumerThread consumerThread = new ConsumerThread();
+
+        startAll(consumerThread, producerThread);
+        joinAll(consumerThread, producerThread);
+    }
+
+    class ConsumerThread extends TestThread {
+        private MultiversionedTransaction dummmyTransaction;
+
+        public ConsumerThread() {
+            super("ConsumerThread");
+        }
+
+        @Override
+        public void run() {
+            dummmyTransaction = stm.startTransaction();
+
+            while (consumerCountdown.decrementAndGet() >= 0) {
+                runTransaction();
+                log();
+            }
+        }
+
+        public void runTransaction() {
+            boolean succes = false;
+            do {
+                MultiversionedHeapSnapshot snapshot = heap.getActiveSnapshot();
+                try {
+                    CommitResult result = runInsideTransaction(snapshot);
+                    if (result.isSuccess())
+                        succes = true;
+                } catch (RetryError e) {
+                    Latch latch = new CheapLatch();
+                    heap.listen(snapshot, latch, new long[]{handle});
+                    latch.awaitUniterruptibly();
+                }
+            } while (!succes);
+        }
+
+        public CommitResult runInsideTransaction(MultiversionedHeapSnapshot snapshot) {
+            Stack stack = (Stack) snapshot.read(handle).___inflate(dummmyTransaction);
+            stack.pop();
+            return heap.commit(snapshot, new InstanceIterator(stack));
+        }
+
+        private void log() {
+            long count = consumerCountdown.get();
+
+            if (count % 1000000 == 0)
+                System.out.printf("Consumer transactions to go %s\n", count);
+        }
+    }
+
+    class ProducerThread extends TestThread {
+        private MultiversionedTransaction dummmyTransaction;
+
+        public ProducerThread() {
+            super("ProducerThread");
+        }
+
+
+        @Override
+        public void run() {
+            dummmyTransaction = stm.startTransaction();
+
+            while (producerCountdown.decrementAndGet() >= 0) {
+                runTransaction();
+                log();
+            }
+        }
+
+        private void log() {
+            long count = producerCountdown.get();
+
+            if (count % 1000000 == 0)
+                System.out.printf("Producer transactions to go %s\n", count);
+        }
+
+        public void runTransaction() {
+            boolean succes = false;
+            do {
+                MultiversionedHeapSnapshot snapshot = heap.getActiveSnapshot();
+                CommitResult result = runInsideTransaction(snapshot);
+                if (result.isSuccess())
+                    succes = true;
+            } while (!succes);
+        }
+
+        public CommitResult runInsideTransaction(MultiversionedHeapSnapshot snapshot) {
+            Stack stack = (Stack) snapshot.read(handle).___inflate(dummmyTransaction);
+            stack.push(item);
+            return heap.commit(snapshot, new InstanceIterator(stack));
+        }
+    }
+}
