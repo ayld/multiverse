efbc7beda73c1eb882c46cdd00c5e343e1fd4216
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2009-03-09 10:39:55 +0000

    - added an additional test for the aba problem

--- trunk/src/test/org/codehaus/multiverse/multiversionedstm/tests/AbaProblemIsNotDetectedTest.java
+++ trunk/src/test/org/codehaus/multiverse/multiversionedstm/tests/AbaProblemIsNotDetectedTest.java
@@ -2,16 +2,14 @@ package org.codehaus.multiverse.multiversionedstm.tests;
 
 import static org.codehaus.multiverse.TestUtils.commit;
 import org.codehaus.multiverse.core.Transaction;
-import org.codehaus.multiverse.core.WriteConflictError;
 import org.codehaus.multiverse.multiversionedheap.standard.DefaultMultiversionedHeap;
 import org.codehaus.multiverse.multiversionedstm.MultiversionedStm;
 import org.codehaus.multiverse.multiversionedstm.examples.Reference;
-import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
 /**
- * A test that shows that the ABA problem is detected.
+ * A test that shows that the ABA problem is not detected.
  * <p/>
  * The ABA problem occurrs when the following sequence happens:
  * <ol>
@@ -23,16 +21,19 @@ import org.junit.Test;
  * Since the value is still the same from the perspective of the first transaction (it has changed from
  * "A" to "B" back to "A" again) the question remains what to do. Should to be a problem or not.
  * <p/>
- * The {@link DefaultMultiversionedHeap} doesn't check the content, but works on versions. If newest
- * version of the data is higher than the version of the data the transaction began with, a writeconflict is
- * detected. So the DefaultMultiversionedHeap does detect the aba problem.
+ * The stm object do dirty checks, so the second transaction that does the A->B and B-A doesn't write
+ * any change because reference is not marked as dirty.
  * <p/>
  * For more information see:
  * http://en.wikipedia.org/wiki/ABA_problem
  *
  * @author Peter Veentjer.
  */
-public class AbaProblemIsDetectedTest {
+public class AbaProblemIsNotDetectedTest {
+    private static final String A = "A";
+    private static final String B = "B";
+    private static final String C = "C";
+
     private MultiversionedStm stm;
     private long handle;
     private DefaultMultiversionedHeap heap;
@@ -41,7 +42,7 @@ public class AbaProblemIsDetectedTest {
     public void setUp() {
         heap = new DefaultMultiversionedHeap();
         stm = new MultiversionedStm(heap);
-        handle = commit(stm, new Reference("A"));
+        handle = commit(stm, new Reference(A));
     }
 
     @Test
@@ -51,19 +52,11 @@ public class AbaProblemIsDetectedTest {
 
         Transaction t2 = stm.startTransaction();
         Reference<String> r2 = (Reference<String>) t2.read(handle);
-        r2.set("B");
+        r2.set(B);
+        r2.set(A);
         t2.commit();
 
-        Transaction t3 = stm.startTransaction();
-        Reference<String> r3 = (Reference<String>) t3.read(handle);
-        r3.set("A");
-        t3.commit();
-
-        r1.set("C");
-        try {
-            t1.commit();
-            fail();
-        } catch (WriteConflictError e) {
-        }
+        r1.set(C);
+        t1.commit();
     }
 }
---
+++ trunk/src/test/org/codehaus/multiverse/multiversionedstm/tests/AbaProblemOverMultipleTransactionIsDetectedTest.java
@@ -0,0 +1,53 @@
+package org.codehaus.multiverse.multiversionedstm.tests;
+
+import static org.codehaus.multiverse.TestUtils.commit;
+import org.codehaus.multiverse.core.Transaction;
+import org.codehaus.multiverse.core.WriteConflictError;
+import org.codehaus.multiverse.multiversionedheap.standard.DefaultMultiversionedHeap;
+import org.codehaus.multiverse.multiversionedstm.MultiversionedStm;
+import org.codehaus.multiverse.multiversionedstm.examples.Reference;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AbaProblemOverMultipleTransactionIsDetectedTest {
+
+    private static final String A = "A";
+    private static final String B = "B";
+    private static final String C = "C";
+
+    private MultiversionedStm stm;
+    private long handle;
+    private DefaultMultiversionedHeap heap;
+
+    @Before
+    public void setUp() {
+        heap = new DefaultMultiversionedHeap();
+        stm = new MultiversionedStm(heap);
+        handle = commit(stm, new Reference(A));
+    }
+
+    @Test
+    public void test() {
+        Transaction t1 = stm.startTransaction();
+        Reference<String> r1 = (Reference<String>) t1.read(handle);
+
+        Transaction t2 = stm.startTransaction();
+        Reference<String> r2 = (Reference<String>) t2.read(handle);
+        r2.set(B);
+        t2.commit();
+
+        Transaction t3 = stm.startTransaction();
+        Reference<String> r3 = (Reference<String>) t3.read(handle);
+        r3.set(B);
+        t3.commit();
+
+        r1.set(C);
+        try {
+            t1.commit();
+            fail();
+        } catch (WriteConflictError er) {
+
+        }
+    }
+}
