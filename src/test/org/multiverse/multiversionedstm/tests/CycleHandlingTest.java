046b361bbd5f049c012733e751b8373c46bfe0bb
john.van.schie <john.van.schie@0d2dd982-918f-11dd-b320-216bd3396a8e>
2009-05-20 22:52:10 +0000

    Added initial implementation for createComplexGraphWithLoadsOfCycles(int) that generates a random graph that might contain cycles.

--- src/test/org/multiverse/multiversionedstm/tests/CycleHandlingTest.java
+++ src/test/org/multiverse/multiversionedstm/tests/CycleHandlingTest.java
@@ -1,5 +1,8 @@
 package org.multiverse.multiversionedstm.tests;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -108,7 +111,7 @@ public class CycleHandlingTest {
     /**
      * A large number is chosen to make sure that there is no hidden recursion in the system.
      */
-    //@Test
+    @Test
     public void complexObjectGraphWithLoadsOfCycles() {
         ComplexNode original = createComplexGraphWithLoadsOfCycles(1000000);
 
@@ -120,8 +123,71 @@ public class CycleHandlingTest {
     }
 
 
+    /**
+     * Create a complex graph that can contain cycles. The maximum amount of nodes
+     * allocated are controlled by the nodeCount parameter, but less could be allocated, 
+     * depending on the random generation of the graph.
+     * @param nodeCount The maximum amount of ComplexNode objects allocated.
+     * @return A graph of ComplexNodes that can contain cycles.
+     */
     private ComplexNode createComplexGraphWithLoadsOfCycles(int nodeCount) {
-        return null;
+    	
+    	if (nodeCount <= 0)
+    		throw new IllegalArgumentException("nodeCount parameter should be > 0.");
+    	
+    	final Random rng = new Random();
+    	
+    	// The chance for the generation of a reference to a previous node.
+    	final double referenceProbability = 0.1;
+    	// The chance for the generation of a terminator node.
+    	final double nullProbability = 0.2;
+    	// Maximum amount of previously generated nodes stored for back referencing.
+    	final int maxReferenceListSize = 12;   	
+    	
+    	// The workList contains all generated ComplexNodes that have no child nodes yet
+    	// and are waiting for processing.
+    	LinkedList<ComplexNode> workList = new LinkedList<ComplexNode>();
+    	// The backreferences contains the list of ComplexNodes that can get referenced by
+    	// a ComplexNode that is processed.
+    	LinkedList<ComplexNode> backreferences = new LinkedList<ComplexNode>();
+    	
+    	// Initialize the lists with the root node.
+    	ComplexNode root = new ComplexNode();
+		--nodeCount;
+    	workList.addFirst(root);    
+    	backreferences.addFirst(root);
+    	
+    	while (!workList.isEmpty() && nodeCount > 0) {
+    		ComplexNode current = workList.removeLast();		
+    		ComplexNode[] children = new ComplexNode[3];
+    		
+    		for (int i = 0; i < 3; ++i) { 
+    			double randomVal = rng.nextDouble();
+    			if (randomVal <= nullProbability) {
+    				children[i] = null;
+    			} else if (randomVal <= (nullProbability + referenceProbability) || nodeCount <= 0) {
+    				// We choose to generate a back reference when we are not allowed to allocate new ComplexNodes
+    				// in order to increase the complexity.
+    				children[i] = backreferences.peekLast();
+    			} else {
+    				children[i] = new ComplexNode();
+    				workList.push(children[i]);
+    				backreferences.addFirst(children[i]);
+    				--nodeCount;
+    				if (backreferences.size() > maxReferenceListSize)
+    					backreferences.removeLast();
+    			}
+    		}
+    		
+    		current.setEdge1(children[0]);
+    		current.setEdge2(children[1]);
+    		current.setEdge3(children[2]);
+    	}
+    	
+    	// After the loop, it could be that the workList is not empty and still many ComplexNodes would
+    	// like to get processed. We can safely ignore these nodes, as their children are initialized to null
+    	// (terminator node) by default.
+    	return root;
     }
 
     @TmEntity
