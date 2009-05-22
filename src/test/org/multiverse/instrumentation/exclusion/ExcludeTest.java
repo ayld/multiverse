ee64a5ed61ff4cf168418b65806a4786f2320335
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2009-05-22 17:02:53 +0000

    - added the excludetest

---
+++ src/test/org/multiverse/instrumentation/exclusion/ExcludeTest.java
@@ -0,0 +1,45 @@
+package org.multiverse.instrumentation.exclusion;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import static org.multiverse.TestUtils.commitAndRead;
+import org.multiverse.api.annotations.Exclude;
+import org.multiverse.api.annotations.TmEntity;
+import org.multiverse.multiversionedstm.MultiversionedStm;
+
+public class ExcludeTest {
+    private MultiversionedStm stm;
+
+    @Before
+    public void setUp() {
+        stm = new MultiversionedStm();
+    }
+
+    @Test
+    public void test() {
+        SomeEntity original = new SomeEntity();
+        original.excluded = 10;
+        original.included = 25;
+
+        SomeEntity found = commitAndRead(stm, original);
+        assertEquals(0, found.excluded);
+        assertEquals(25, found.included);
+
+        found.excluded = 20;
+        //todo: as soon as the isDirty
+        //assertFalse(((MaterializedObject) found).isDirty());
+
+        found.included = 100;
+        //assertTrue(((MaterializedObject) found).isDirty());
+    }
+
+    @TmEntity
+    public static class SomeEntity {
+
+        @Exclude
+        private int excluded;
+
+        private int included;
+    }
+}
