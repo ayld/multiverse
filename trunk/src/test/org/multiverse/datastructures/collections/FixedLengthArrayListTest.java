65bee16c05328b3b1f3d5ab4cfdd7328b2dd2589
alarmnummer <alarmnummer@0d2dd982-918f-11dd-b320-216bd3396a8e>
2009-09-18 10:14:25 +0000

    - added initial support for an array based list

---
+++ trunk/src/test/org/multiverse/datastructures/collections/FixedLengthArrayListTest.java
@@ -0,0 +1,53 @@
+package org.multiverse.datastructures.collections;
+
+import static junit.framework.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import org.multiverse.stms.alpha.AlphaStm;
+import org.multiverse.utils.GlobalStmInstance;
+
+import java.util.List;
+
+public class FixedLengthArrayListTest {
+    private AlphaStm stm;
+
+    @Before
+    public void setUp() {
+        stm = new AlphaStm();
+        GlobalStmInstance.set(stm);
+    }
+
+    // ==================================
+
+    @Test
+    public void addOnEmptyList() {
+        List<String> list = new FixedLengthArrayList<String>(10);
+        long version = stm.getClockVersion();
+
+        list.add("foo");
+
+        assertEquals(version + 1, stm.getClockVersion());
+        assertEquals(1, list.size());
+        assertEquals("foo", list.get(0));
+    }
+
+    @Test
+    public void addOnNonEmptyList() {
+        List<String> list = new FixedLengthArrayList<String>(10);
+        list.add("1");
+        list.add("2");
+
+        long version = stm.getClockVersion();
+
+        list.add("3");
+
+        assertEquals(version + 1, stm.getClockVersion());
+        assertEquals(3, list.size());
+        assertEquals("1", list.get(0));
+        assertEquals("2", list.get(1));
+        assertEquals("3", list.get(2));
+    }
+
+
+    // ==================================
+}
